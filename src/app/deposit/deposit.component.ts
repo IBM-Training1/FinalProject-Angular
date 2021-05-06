c9e2dcad772ea5f7726036f877f5a4a6fd6ff490
unknown <ibmjavanh05@iiht.tech>
2021-05-06 18:33:50 +0530

    Deposit page

--- src/app/deposit/deposit.component.ts
+++ src/app/deposit/deposit.component.ts
@@ -1,15 +1,73 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Account } from '../Account';
+import { AccountService } from '../account.service';
 @Component({
   selector: 'app-deposit',
   templateUrl: './deposit.component.html',
   styleUrls: ['./deposit.component.css']
 })
 export class DepositComponent implements OnInit {
+  title: String = "Update Account Details";
+  account: Account = new Account();
+  accountArray: any;
+  currentStatus: any;
 
-  constructor() { }
+
+  constructor(private accountService: AccountService) { }
+
+  searchAccountByNumber(number: any) {
+
+    let URL = 'http://localhost:8081/account/';
+    let accountNumber = (<HTMLInputElement>document.getElementById('number')).value;
+    if (accountNumber) {
+      URL = URL + 'number/' + accountNumber;
+      const observable = this.accountService.searchAccountByNumber1(accountNumber);
+      observable.subscribe(response => {
+        this.accountArray = response;
+        this.currentStatus = this.accountArray.status;
+        console.log("sucess");
+        if (this.accountArray) {
+          this.account = this.accountArray
+        }
+        else {
+          alert("Enter a valid account number");
+        }
+
+      },
+        (error: any) => {
+          console.log(error);
+          alert("error");
+        }
+
+      )
+
+    }
+    else {
+      alert("Please enter account number");
+    }
+
+  }
+
+  deposit() {
+    const promise = this.accountService.updateAccount(this.account, this.account.id);
+    promise.subscribe((response: any) => {
+      console.log(response);
+
+      this.account.balance+=this.account.depositAmount;
+      this.accountArray[response];
+
+      alert("Amount Deposited")
+    },
+
+      error => {
+        console.log(error);
+        alert("Failed!");
+
+      })
+  }
 
   ngOnInit(): void {
   }
+
 
 }
