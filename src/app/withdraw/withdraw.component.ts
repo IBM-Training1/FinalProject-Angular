1bf00700717011c2c228b8d2f95ac5a282bc76e3
unknown <ibmjavanh11@iiht.tech>
2021-05-06 18:28:14 +0530

    added withdraw method

--- src/app/withdraw/withdraw.component.ts
+++ src/app/withdraw/withdraw.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Account } from '../Account';
+import { AccountService } from '../account.service';
 
 @Component({
   selector: 'app-withdraw',
@@ -6,8 +8,56 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./withdraw.component.css']
 })
 export class WithdrawComponent implements OnInit {
+  title: String = "Update Account Details";
+  account: Account = new Account();
+  accountArray: any;
+  currentStatus: any;
 
-  constructor() { }
+  constructor(private accountService: AccountService) { }
+
+  searchAccountByNumber(number: any) {
+
+    let URL = 'http://localhost:8081/account/';
+    let accountNumber = (<HTMLInputElement>document.getElementById('number')).value;
+    if (accountNumber) {
+      URL = URL + 'number/' + accountNumber;
+      const observable = this.accountService.searchAccountByNumber1(accountNumber);
+      observable.subscribe(response => {
+        this.accountArray = response;
+        this.currentStatus = this.accountArray.status;
+        console.log("success");
+        if (this.accountArray) {
+          this.account = this.accountArray
+        }
+        else {
+          alert("Enter a valid account number");
+        }
+      },
+        (error: any) => {
+          console.log(error);
+          alert("error");
+        })
+    }
+    else {
+      alert("Please enter account number");
+    }
+  }
+
+  update() {
+    const promise = this.accountService.updateAccount(this.account, this.account.id);
+    promise.subscribe((response: any) => {
+      console.log(response);
+      this.accountArray[response];
+
+      alert("Account is Updated")
+    },
+
+      error => {
+        console.log(error);
+        alert("Update not possible");
+
+      })
+  }
 
   ngOnInit(): void {
   }
